3d1ed7aba5204f563ba4518c36f84c3d2081fe51
Rapaix <rafaelpx8812@gmail.com>
2021-08-31 01:46:09 -0300

    adiciona modelo tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_025325) do
+ActiveRecord::Schema.define(version: 2021_08_31_030824) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
+  enable_extension "plpgsql"
 
   create_table "contents", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.citext "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name", "user_id"], name: "index_tags_on_name_and_user_id", unique: true
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_08_31_025325) do
   end
 
   add_foreign_key "contents", "users"
+  add_foreign_key "tags", "users"
 end
