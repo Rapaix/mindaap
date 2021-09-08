d952ae421b8caf455d2d65b720beabf3a8c71cb1
Rapaix <rafaelpx8812@gmail.com>
2021-09-07 23:37:25 -0300

    Aciona tela de ediçao de conteudo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -4,7 +4,27 @@ class ContentsController < ApplicationController
     @contents = current_user.contents
   end
 
+  # toma cuidado com a grafia dos atributos
   def new
-    @contents = Content.new
+    @content = Content.new
+  end
+
+  def create
+    # byebug
+    @content = current_user.contents.build(content_params)
+
+    if @content.save
+      redirect_to contents_path, notice: 'Content Sucessfully created'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @content = Content.find(params[:id])
+  end
+
+  def content_params
+    params.require(:content).permit(:title, :description)
   end
 end
